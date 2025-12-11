185305cb4115ed6cabad3080a6227d93c8851487
1LeandroLV <leandro.verdugo02@gmail.com>
2025-12-11 01:41:32 +0100

    Added working menu loop with switch and user input

---
+++ Budgetmangerrr.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budgetmanger
+{
+    internal class Budgetmangerrr
+    {
+       private  List<Transaction> transactions;
+       public void AddTransaction() 
+        { 
+        }
+        public void ShowAll() 
+        {  
+        
+        }
+        public void CalculateBalance() 
+        { 
+        }
+        public void DeleteTrasaction()
+        { 
+        
+        }
+                   
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Budgetmangerrr mybudgetmanger = new Budgetmangerrr();
+            bool altenativ = true;
+            while (altenativ)
+            {
+              string menyval = Console.ReadLine();
+                switch (menyval)
+                {
+                    case "1":
+                        mybudgetmanger.AddTransaction();
+                        break;
+                    case "2":
+                        mybudgetmanger.ShowAll();
+                        break;
+                    case "3":
+                        mybudgetmanger.CalculateBalance();
+                        break;
+                    case "4":
+                        mybudgetmanger.DeleteTrasaction();
+                        break;
+                    case "5":
+                         altenativ = false;
+                        break;
+                    default:
+                     Console.WriteLine("Fel val, va vänlig och försök igen");
+                     break;
+                }
+            }
+            Console.WriteLine("Välkommen till Personal budget meny");
+            Console.WriteLine("Välj ett Alnativ nedan!");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("1: Lägg till transaktion");
+            Console.WriteLine("2: Visa alla transaktioner");
+            Console.WriteLine("3: Visa total balans");
+            Console.WriteLine("4: Ta bort Transaktion");
+            Console.WriteLine("5: Avsluta programmet");
         }
     }
 }
